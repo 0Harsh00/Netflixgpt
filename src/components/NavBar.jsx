1a568a84e0685cdd580e8122ca0a7ffd04f52631
0Harsh00 <lalshakhu@gmail.com>
2024-11-05 19:36:02 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React  from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=''>
+        <div className="logo">
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/aad37504-bbe0-407d-b8a8-fb31b8faf374/web_tall_panel/IN-en-20241028-TRIFECTA-perspective_2f496d82-4466-44ef-b581-9d69138a2914_large.jpg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
